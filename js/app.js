e6b851c0f01ffe80e884e7ea28fd889c7dc9dd4f
Muhammet Koc <muhammetkoc874@gmail.com>
2023-04-08 21:56:43 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,18 +14,18 @@ function init() {
     //create scene
     scene = new THREE.Scene();
     const fov = 35;
-    const aspect = container.clientWidth /container.clientHeight;
+    const aspect = container.clientWidth / container.clientHeight;
     const near = 0.1;
     const far = 500;
 
     //camera Setup
-    camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-    camera.position.set(3,10,60);
-    const ambient = new THREE.AmbientLight(0x404040,5);
-    scene.add(ambient); 
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(3, 10, 60);
+    const ambient = new THREE.AmbientLight(0x404040, 5);
+    scene.add(ambient);
 
     //renderer
-    renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 
@@ -33,27 +33,27 @@ function init() {
 
     //load model
     let loader = new THREE.GLTFLoader();
-    loader.load('assets/3d/modelv7.glb', function(gltf){
-        scene.add(gltf.scene); 
+    loader.load('./assets/3d/namev5.glb', function (gltf) {
+        scene.add(gltf.scene);
         model = gltf.scene.children[0];
         animate();
 
     });
 }
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
-   // model.rotation.y += 0.01;
-  //  model.rotation.x += 0.01;
+    // model.rotation.y += 0.01;
+    //  model.rotation.x += 0.01;
     model.rotation.z += 0.008;
     renderer.render(scene, camera);
 }
 
 init()
 
-function onWindowResize(){
-    camera.aspect = container.clientWidth/container.clientHeight;
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(container.clientWidth,container.clientHeight);
+    renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
-window.addEventListener('resize',onWindowResize);
+window.addEventListener('resize', onWindowResize);
